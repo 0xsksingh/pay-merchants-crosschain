b6b8690ff90672fedeed4e53070e9b0d270a8fff
kamalbuilds <geniusamansingh@gmail.com>
2024-08-11 21:15:12 +0530

    add the rollup address

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -11,9 +11,10 @@ export const siteConfig = {
     docs: "https://ui.shadcn.com",
   },
   crosspayaddress: {
-    84532: "0xC0204113CcC43D83De06191C082ea696C65466f9", // Base Sepolia
+    84532: "0x28d0F2353af7e357Eed9f960D1D2213Fa85a270e", // Base Sepolia
     56: "0xc8E2B6AC668AC47ffE8814E86aDCb966C6AA3d5b", // Mode Sepolia
     11155420: "0x2AAC535db31DB35D13AECe36Ea7954A2089D55bE", // OP Sepolia
-    2522: "0x301Ab38c7f652FA23C7Ba1fa182E36665Dac0fC2" // fRAXTAL Holesky
+    2522: "0x301Ab38c7f652FA23C7Ba1fa182E36665Dac0fC2", // fRAXTAL Holesky
+    1020: "0x2AAC535db31DB35D13AECe36Ea7954A2089D55bE" // Conduit Rollup
   }
 }
--- frontend/utils/eas-wagmi-utils.ts
+++ frontend/utils/eas-wagmi-utils.ts
@@ -1,6 +1,6 @@
 import { Config, getConnectorClient, getClient } from '@wagmi/core'
 import { BrowserProvider, FallbackProvider, JsonRpcProvider, JsonRpcSigner } from 'ethers'
-import type { Account, Chain, Client, Transport } from 'viem'
+import { defineChain, type Account, type Chain, type Client, type Transport } from 'viem'
 
 export function clientToSigner(client: Client<Transport, Chain, Account>) {
     const { account, chain, transport } = client
@@ -49,3 +49,76 @@ export function getEthersProvider(
     if (!client) return
     return clientToProvider(client)
 }
+
+export const conduitPaytoMerchantRollup = defineChain({
+    id: 1020,
+    name: 'PaytoMerchantsRollup',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://rpc-paytomerchants-co4xglkw0h.t.conduit.xyz'],
+        webSocket: ['wss://rpc-paytomerchants-co4xglkw0h.t.conduit.xyz'],   
+      },
+    },
+    blockExplorers: {
+      default: { name: 'Explorer', url: 'https://explorer-paytomerchants-co4xglkw0h.t.conduit.xyz' },
+    },
+    contracts: {
+    crosspayaddress: {
+        address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+        blockCreated: 5882,
+      },
+    },
+  })
+
+export const modeSepolia = defineChain({
+    id: 56,
+    name: 'ModeSepolia',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://sepolia.mode.network'],  
+      },
+    },
+    blockExplorers: {
+      default: { name: 'Explorer', url: 'https://explorer-modesepl-co4xglkw0h.t.conduit.xyz' },
+    },
+    contracts: {
+        crosspayaddress: {
+            address: '0x2AAC535db31DB35D13AECe36Ea7954A2089D55bE',
+            blockCreated: 5882,
+        },
+    },
+  })
+
+export const fraxtaltestnet = defineChain({
+    id: 2522,
+    name: 'FraxtalTestnet',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://rpc.testnet.frax.com'], 
+      },
+    },
+    blockExplorers: {
+      default: { name: 'Explorer', url: 'https://holesky.fraxscan.com/' },
+    },
+    contracts: {
+        crosspayaddress: {
+            address: '0x301Ab38c7f652FA23C7Ba1fa182E36665Dac0fC2',
+            blockCreated: 5882,
+        },
+    },
+  })
